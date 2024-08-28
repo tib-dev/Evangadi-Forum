966734c20c5b90fa474e11cde80a5eabca8c5dcb
Tibebu K. <132199829+tib-dev@users.noreply.github.com>
2024-08-28 03:09:45 +0300

    Update Header.jsx

--- Front-End/evangadi-forum/src/Components/Header/Header.jsx
+++ Front-End/evangadi-forum/src/Components/Header/Header.jsx
@@ -6,52 +6,63 @@ import { userProvider } from "../../Context/UserProvider";
 import "./Header.css";
 
 function Header({ logOut }) {
-	const [user, setUser] = useContext(userProvider);
-	const navigate = useNavigate();
+  const [user, setUser] = useContext(userProvider);
+  const navigate = useNavigate();
 
-	function handleButtonClick() {
-		if (user.userName) {
-			logOut();
-		} else {
-			navigate("/secondPage");
-		}
-	}
+  function handleButtonClick() {
+    if (user.userName) {
+      logOut();
+    } else {
+      navigate("/secondPage");
+    }
+  }
 
-	return (
-		<Navbar expand="lg" className="navbar" fixed="top" variant="dark">
-			<Container>
-				<Navbar.Brand as={Link} to="/">
-					<img src={logo} alt="Logo" className="navbar-logo" />
-				</Navbar.Brand>
-				<Navbar.Toggle
-					aria-controls="basic-navbar-nav"
-					className="navbar-toggler-icon"
-				>
-					<span>
-						<i
-							className="fas fa-bars"
-							style={{ color: "black", fontSize: "2em" }}
-						></i>
-					</span>
-				</Navbar.Toggle>
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="ms-auto">
-						<Nav.Link as={Link} to="/" className="black link blakkk">
-							Home
-						</Nav.Link>
-						<Nav.Link as={Link} to="/how-it-works" className="black link">
-							How it Works
-						</Nav.Link>
-					</Nav>
-					<Nav className="m-0 m-md-3">
-						<button className="btn btn-success" onClick={handleButtonClick}>
-							{user.userName ? "Log Out" : "Sign In"}
-						</button>
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
-	);
+  function handleHomeClick() {
+    if (user.userName) {
+      navigate("/home"); // Redirect to the home (question and answer) page if user is logged in
+    } else {
+      navigate("/"); // Otherwise, redirect to the landing page
+    }
+  }
+
+  return (
+    <Navbar expand="lg" className="navbar" fixed="top" variant="dark">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img src={logo} alt="Logo" className="navbar-logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          className="navbar-toggler-icon"
+        >
+          <span>
+            <i
+              className="fas fa-bars"
+              style={{ color: "black", fontSize: "2em" }}
+            ></i>
+          </span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link
+              className="black link blakkk"
+              onClick={handleHomeClick}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/how-it-works" className="black link">
+              How it Works
+            </Nav.Link>
+          </Nav>
+          <Nav className="m-0 m-md-3">
+            <button className="btn btn-success" onClick={handleButtonClick}>
+              {user.userName ? "Log Out" : "Sign In"}
+            </button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Header;
